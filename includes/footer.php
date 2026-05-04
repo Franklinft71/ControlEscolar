dc132d0a697f9db4a9182ae2e5da8638eb371dea
Franklin Taylhardat <91097474+Franklinft71@users.noreply.github.com>
2026-05-03 21:52:09 -0400

    Replace local script sources with CDN links

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,9 @@
 </div> <!-- End Main Content -->
 </div> <!-- End d-flex wrapper -->
-
-<script src="<?php echo APP_URL; ?>/vendor/components/jquery/jquery.min.js"></script>
-<script src="<?php echo APP_URL; ?>/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-<script src="<?php echo APP_URL; ?>/vendor/sweetalert2/sweetalert2.all.min.js"></script>
-<script src="<?php echo APP_URL; ?>/vendor/select2/dist/js/select2.min.js"></script>
-
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.7/dist/sweetalert2.all.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <!-- Script base para App global -->
 <script>
     // Objeto global de la aplicación
