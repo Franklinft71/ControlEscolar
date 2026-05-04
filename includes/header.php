bac70b48ffb15af4f36cbfd4e3dd5d681df00101
Franklin Taylhardat <91097474+Franklinft71@users.noreply.github.com>
2026-05-03 21:48:14 -0400

    Include external JS libraries in header.php
    Added script tags for jQuery, Bootstrap, SweetAlert2, and Select2.

--- includes/header.php
+++ includes/header.php
@@ -19,18 +19,10 @@ if (!isset($_SESSION['usuario_id'])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php echo isset($page_title) ? $page_title . ' - ' . APP_NAME : APP_NAME; ?></title>
-
-  <!-- Bootstrap CSS -->
-  <link href="<?php echo APP_URL; ?>/vendor/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-  <!-- FontAwesome -->
-  <link rel="stylesheet" href="<?php echo APP_URL; ?>/vendor/fontawesome/css/all.min.css">
-  <!-- SweetAlert2 -->
-  <link href="<?php echo APP_URL; ?>/vendor/sweetalert2/sweetalert2.css" rel="stylesheet">
-  <!-- Select2 -->
-  <link href="<?php echo APP_URL; ?>/vendor/select2/dist/css/select2.min.css" rel="stylesheet" />
-  <link href="<?php echo APP_URL; ?>/vendor/select2/dist/css/select2-bootstrap-5-theme.min.css" rel="stylesheet" />
-  <!-- Custom CSS -->
-  <link rel="stylesheet" href="<?php echo APP_URL; ?>/assets/css/style.css">
+  <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 </head>
 
 <body class="bg-light">
